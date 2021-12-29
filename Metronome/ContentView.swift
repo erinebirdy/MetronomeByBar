0d742e7e4cc85c0a1276a11b9eecabbee7a69b21
Erin <erin.liz.baird@gmail.com>
2021-12-28 23:15:24 -0700

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by Erin Baird on 2021-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Erin Baird on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
